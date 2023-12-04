4a18e3fabb71d826ce99d3f79074731821cfece4
karangoraniya <karangoraniya@gmail.com>
2023-12-04 12:43:56 +0530

    feat : package id for woo_commerce, e_commerce & defi added

---
+++ src/utils/utils.ts
@@ -0,0 +1,19 @@
+export const SUI_ADDRESS_REGEX = /^0x[a-fA-F0-9]{64}$/;
+export const TESTNET_PACKAGE_ID =
+  "0x2b4232ac9939d5569a8ef75f951c8448749b02a25143000d89c336238ec69558";
+export const DEVNET_PACKAGE_ID =
+  "0xabb72e403136a2a13be2a3bd77998e7a9777c7f00b05e1a9fc9eac3cd31e70dc";
+export const MAINNET_PACKAGE_ID =
+  "0x96559b97b00b684f46241ad9b4820c869ff93333766d6729f3d747d4bc9d5f00";
+export const IDO_PACKAGE_TESTNET =
+  "0xe64aa61b4132ae241b29cda275bff617f015f25dc0d91ad831a87a56f654fc75";
+export const SUI_WOOCOMMERCE_TESTNET =
+  "0xcb76d006828cf6f5afbc8885a62ce91f1127ce71064deb2b8b1ffab3896a2c29";
+export const SUI_WOOCOMMERCE_TREASURY =
+  "0xf0bb44558fc7a21a0a3772f5459a05fb574d7e5ecc85cb5eaa80f1a2e619327e";
+export const PHASE_201 = 201; //"Transaction Success";
+export const PHASE_202 = 202; //"Transaction Fail";
+export const PHASE_203 = 203; //"InsufficientCoinBalance in your wallet";
+export const PHASE_204 = 204; // "No response or transaction data received"
+export const PHASE_205 = 205; //"The shared merchant address is not correct";
+export const COIN_TYPE = "0x2::sui::SUI";
