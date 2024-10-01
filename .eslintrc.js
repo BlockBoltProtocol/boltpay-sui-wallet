49b354a038448c77899b1570d089d0913cce3e75
karangoraniya <support@blockbolt.io>
2024-10-01 19:21:15 +0530

    chore : eslint & prettierrc added

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+export default {
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+  ],
+  rules: {
+  },
+  };
